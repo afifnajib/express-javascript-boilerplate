2d8d9d24f17ebcd9f223095f5d0f3123680845de
afifnajib <afifnajib1234@gmail.com>
2023-12-20 16:11:27 +0700

    modify swagger

--- src/routes/v1/profile.route.js
+++ src/routes/v1/profile.route.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const auth = require("../../middlewares/auth");
-const validate = require("../../middlewares/validate");
-const userValidation = require("../../validations/user.validation");
+// const validate = require("../../middlewares/validate");
+// const userValidation = require("../../validations/user.validation");
 const userController = require("../../controllers/user.controller");
 
 const router = express.Router();
